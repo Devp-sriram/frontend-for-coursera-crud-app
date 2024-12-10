e556ff4ff619286f833ed15577425fd98c19dbbc
Devp-sriram <sriramraman100@gmail.com>
2024-12-10 22:29:09 +0530

    next config added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 
 
 export default function Home() {
-  return 
-  <div className="border-gray-500 justify-center items-center ">
+  return
+    <>  
+      <div className="border-gray-500 justify-center items-center ">
       <h1 className="text-2xl font-bold white ">Welcome to the employee management app</h1>
       
   </div>
+  </>
 }
 
